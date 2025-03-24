872ca2dd20221962e71c1cbc47c34c55cd962d1c
ujjval sharma <ujjvalsharma007@gmail.com>
2025-03-24 23:33:55 +0000

    variabal and constant

---
+++ 0_1basics/variable.js
@@ -0,0 +1,21 @@
+const accountId = 6438
+let accountEmail = "ujjvalsharma@gmail.com"
+var accountPassword = "438443"
+acountCity = "noida"
+
+
+//accountId = 8934  //not allowed 
+
+console.log(accountId);
+
+accountEmail = "ujjipandat@gmail.com"
+accountPassword = "37847"
+acountCity;
+
+/*
+ prefer not to use var 
+ because of the issue in block scope and functional socpe 
+*/
+
+
+console.table([accountEmail , accountPassword , acountCity]);
